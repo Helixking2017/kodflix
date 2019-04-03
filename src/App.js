e9c96ddf5bce9ac865106f67e6f740b9e2335b4b
Wonder <wonderman01@gmail.com>
2019-04-03 20:54:29 +0100

    movie image part 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import heatMovImg from './heatImg.jpg';
+import heatMovImg from './Images/heatImg.jpg';
+import BerserkImg from './Images/Berserk.jpg';
+import CodeGeassImg from './Images/Code Geass.jpg';
+import FullMetalAlchemistImg from './Images/Fullmetal Alchemist.jpg';
+import SamuraiChamplooImg from './Images/Samurai Champloo.jpg';
+import TrigunImg from './Images/Trigun.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={heatMovImg} className="movie" alt=""></img>
-          <p>=43
+        <div className= "container">
+            <div className= "item movie-title"><img src="" </div>
+            <div className= "item movie-title">movie2</div>
+            <div className= "item movie-title">movie3</div>
+          </div>
+          <div className="container"> 
+            <div className= "item movie-title">movie4</div>
+            <div className= "item movie-title">movie5</div>
+            <div className= "item movie-title">movie6</div>
+          </div>
+          <p>
             <h1> Yo! Check out my first website/app project!</h1>
           </p>
           <a
